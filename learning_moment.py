4f1273cbec811ae5ced02fd9eca87f2a42ba3b0d
XinY1315 <xinyit3@nycstudents.net>
2026-09-18 09:20:19 -0400

    the

---
+++ learning_moment.py
@@ -0,0 +1,22 @@
+#i is an incrementor
+# -= and += means subtract or add what's on the right to the og
+#string for characters
+#input asks the user a question and records the answer
+#what we write in input argument is what user sees
+#input ALWAYS outputs a string
+""" bill = int(input("How much was the bill?"))
+print(bill)
+
+if bill == 10:
+    print("Match")
+else:
+    print("no match") """
+
+""" #integer for whole numbers
+amt = 100
+#float for decimals
+amt_two = 99.99 """
+
+#boolean
+""" x = True
+y = False """
--- loops.py
+++ loops.py
@@ -2,17 +2,25 @@ import turtle
 from turtle import *
 t = Turtle()
 
-#i is an incrementor
-# -= and += means subtract or add what's on the right to the og
 
-
-sidelength = 100
+""" sidelength = 100
 rotate = 90
 def square(x,y):
     for i in range(4):
         t.forward(x)
         t.left(y)
-square(100,90)
+square(100,90) """
 
-def triangle(x,y):
+""" def triangle(x,y):
     for i in range(3):
+        t.forward(100)
+        t.left(120)
+triangle(100,100)
+ """
+sidelength = 100
+rotate = 5
+def square(x,y):
+    for i in range(240):
+        t.forward(x)
+        t.left(y)
+square(100,90)
